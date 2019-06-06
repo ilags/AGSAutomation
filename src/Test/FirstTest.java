283cab01d9eeba51e46e2abd3bca9a8a2f557d98
Moshe <mrozenfeld@playags.com>
2019-06-06 11:43:15 +0300

    First commit - setup - moshe

---
+++ src/Test/FirstTest.java
@@ -0,0 +1,33 @@
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+class FirstTest {
+
+    @BeforeAll
+    public static void setup()
+    {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\IdeaProjects\\AGSAutomation\\WebDrivers\\chromedriver.exe");
+    }
+
+
+
+    WebDriver NewDriver = new ChromeDriver();
+    @Test
+    void Rety()
+    {
+        NewDriver.navigate().to("https://www.playags-games.com");
+        NewDriver.manage().window().maximize();
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/Test/WebUIMApping.java
@@ -0,0 +1,39 @@
+
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+
+public class WebUIMApping {
+
+
+
+
+
+
+    public class Header
+    {
+
+        By Id = By.id("");
+
+    }
+
+
+
+
+
+    public class MainSection
+    {
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
