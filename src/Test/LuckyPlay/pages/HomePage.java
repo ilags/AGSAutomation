cb148e157aa0e4e2f9ff31d04b6048c999a70a39
Moshe <mrozenfeld@playags.com>
2019-08-08 17:09:54 +0300

    API Test first try out

--- src/Test/LuckyPlay/pages/HomePage.java
+++ src/Test/LuckyPlay/pages/HomePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By ;
 public class HomePage {
 
 
+  public By NavBar = By.xpath("//*[@id=\"header\"]/div/div[2]");
 
 
 
--- src/Test/LuckyPlay/pages/LoadScreen.java
+++ src/Test/LuckyPlay/pages/LoadScreen.java
@@ -3,4 +3,13 @@ package pages;
 import org.openqa.selenium.By ;
 
 public class LoadScreen {
+
+
+
+    public By GusetButton =  By.xpath("//*[@id=\"content\"]/div/div[2]/div[2]");
+
+
+
+
+
 }
